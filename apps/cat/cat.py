b01a46fb4f1fe2fbc5de8f4fc89e0c3e5cafe638
Ben Gamra Khalil <khalil.ben-gamra@student-cs.fr>
2023-05-30 17:00:03 +0200

    Fonctions chat

---
+++ apps/cat/cat.py
@@ -0,0 +1,12 @@
+
+def scan_environment():
+    # se tourne pour voir s'il y a des robots dans la zone
+
+
+def scan_ligne():
+    # se deplace jusqu'a l'obstacle le plus proche a gauche / a droite
+
+
+def envoie_id_robot_trouvé():
+    # reconnait le robot
+    # envoie l id au lobby?
