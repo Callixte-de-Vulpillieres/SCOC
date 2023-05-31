2537c10f8dc23d6c4001481d2e583477ec51fbb6
Ben Gamra Khalil <khalil.ben-gamra@student-cs.fr>
2023-05-31 16:53:36 +0200

    qq changements

--- apps/cat/cat.py
+++ apps/cat/cat.py
@@ -6,7 +6,9 @@ import os
 
 # setting path
 qrcode_recognition_path = os.path.abspath('../../module/bot-recognition/qrcode_recognition')
+commands_path = os.path.abspath('../../module/commands')
 sys.path.append(qrcode_recognition_path)
+sys.path.append(commands_path)
 
 from qrcode_scanner import *
 
@@ -17,7 +19,13 @@ nb_bots = 5
 cam = cv2.VideoCapture(0)
 cam.set(3, 640)
 cam.set(4, 480)
-fps = 5
+fps = 10
+
+start_time = time.time()
+searching_time = 5
+
+def move():
+    print("moving")
 
 while searching:
     # scan les qrcodes
@@ -26,9 +34,18 @@ while searching:
         for bot in res:
             if bot not in bots_found:
                 bots_found.append(bot)
+                #send to lobby:
+                
                 if len(bots_found) >= nb_bots:
                     searching = False
                     print("I win!")
                     cam.release()
     time.sleep(1/fps)
     print(bots_found)
+    
+    #se bouger le cul
+    if time.time()> start_time + searching_time:
+        start_time = time.time()
+        move()
+        
+
